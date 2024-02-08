b6fc60713e5e09bbebbef4d1bcc1293d13148fed
Edvardas Sneideris <edvardas.sneideris@tutotoons.com>
2024-02-08 13:45:01 +0200

    create ui assembly

--- Assets/Scripts/TrainEditor/TrainSettings.cs
+++ Assets/Scripts/TrainEditor/TrainSettings.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using TrainConstructor.ReusableComponents;
+using TrainConstructor.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
--- Assets/Scripts/UI/ConfirmationPanel.cs
+++ Assets/Scripts/UI/ConfirmationPanel.cs
@@ -3,7 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace TrainConstructor.ReusableComponents
+namespace TrainConstructor.UI
 {
     [RequireComponent(typeof(CanvasGroup))]
     public class ConfirmationPanel : MonoBehaviour
